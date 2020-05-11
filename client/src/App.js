f9e5bfb1ca9af752b00e77b6309c1d4c88cc8bca
Lokesh Pareek <lokeshpareek5@users.noreply.github.com>
2020-05-11 20:17:10 +0530

    React Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React, {Fragment} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
 
-const App = () =>  
-   <Fragment>
-     <NavBar />
-     <Landing />
-   </Fragment>
+const App = () => (
+  <Router>
+    <Fragment>
+      <NavBar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </section>
+    </Fragment>
+  </Router>
+) 
 
 export default App;
