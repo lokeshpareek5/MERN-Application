18547634922a6a9ede48d75096722a2025262414
Lokesh Pareek <lokeshpareek5@users.noreply.github.com>
2020-05-24 19:20:23 +0530

    Reducer, create alert util

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,29 @@ import NavBar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
+  <Provider store={store}>
   <Router>
     <Fragment>
       <NavBar />
       <Route exact path="/" component={Landing} />
       <section className="container">
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
+        <Alert />
+        <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+        </Switch>        
       </section>
     </Fragment>
   </Router>
+  </Provider>
 ) 
 
 export default App;
