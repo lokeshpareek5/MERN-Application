d728aa8bc54ceee436c1d9fbab0a22e71a8cb2fe
Lokesh Pareek <lokeshpareek5@users.noreply.github.com>
2020-07-05 20:52:56 +0530

    Edit Profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile';
 import Spinner from '../layout/Spinner';
 import { Link, Redirect } from 'react-router-dom';
+import DashboardActions from './DashboardActions';
 
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
     useEffect(() => {
@@ -16,7 +17,9 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
             <i className="fas fa-use">Welcome { user && user.name}</i>
         </p>
         {profile !== null ? (
-        <Fragment>has </Fragment>
+        <Fragment>
+            <DashboardActions />
+        </Fragment>
         ) : (
         <Fragment>
             <p>You have not yet setup a profile, please add some info</p>
