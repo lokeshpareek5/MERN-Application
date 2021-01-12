12ba915dd9ea01c09f1cc743546dd41a5398b25f
Lokesh Pareek <lokeshpareek5@users.noreply.github.com>
2021-01-12 20:29:33 +0530

    prepare for deployment

---
+++ client/build/precache-manifest.9a032a58474dbd3ce166e38a24e57ece.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b360832b43c80f3535c52a15db6bd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa92369d45250e350ee8",
+    "url": "/static/css/main.83648ea1.chunk.css"
+  },
+  {
+    "revision": "76cedbf08f885ab32128",
+    "url": "/static/js/2.6eb4f424.chunk.js"
+  },
+  {
+    "revision": "7e6ec0d9861ce35e695c4f042343a82f",
+    "url": "/static/js/2.6eb4f424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa92369d45250e350ee8",
+    "url": "/static/js/main.d29e23bd.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "be0d9fd5a1c00a70c7cc41abd73709ff",
+    "url": "/static/media/spinner.be0d9fd5.gif"
+  }
+]);
